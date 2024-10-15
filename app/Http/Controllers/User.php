b63fee6ad09d14813188d2d7862950287717da5e
Mathis Delmaere <mathis.delmaere@free.fr>
2024-10-15 14:46:09 +0200

    Nouveaux Controllers pour l'Oauth

---
+++ app/Http/Controllers/User.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Enums\Role as RoleEnum;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    /**
+     * Create or update a user in the database
+     *
+     * @param $userDetails
+     * @param $currentAssociations
+     * @return User
+     */
+    public function createOrUpdateUser($userDetails)
+    {
+        $user = User::firstOrCreate(
+            [
+                'id' => $userDetails['uuid'],
+                'firstname' => $userDetails['firstName'],
+                'lastname' => $userDetails['lastName'],
+                'email' => $userDetails['email'],
+            ]
+        );
+
+        return $user;
+    }
+
+    /**
+     * Update language preference of the authenticated user
+     *
+     * @param $language
+     */
+    public static function updateLanguagePreference($language)
+    {
+        // Find the user by ID
+        $user = User::find(Auth::user()->id);
+        $user->language = $language;
+        $user->save();
+
+        return response('', 200);
+    }
+}
