3c055fe0664b1660c2a47acc8f6648befbbfec97
mathisDlmr <mathis.delmaere@free.fr>
2025-07-27 12:42:04 +0200

    refacto pipeline CI

--- laravel-start.sh
+++ laravel-start.sh
@@ -2,17 +2,10 @@
 
 set -eu
 
-chown www-data:www-data /var/www/html/.env
-
-mkdir -p storage/logs storage/framework/cache storage/framework/sessions storage/framework/views
-chown -R www-data:www-data storage
-chmod -R 775 storage
+mkdir -p storage/logs
 touch storage/logs/laravel.log
 chown www-data:www-data storage/logs/laravel.log
 chmod 664 storage/logs/laravel.log
-
-php artisan key:generate --force
-php artisan jwt:generate
 
 php artisan config:clear || true
 php artisan cache:clear || true
@@ -26,7 +19,7 @@ php artisan view:cache --no-interaction
 php artisan event:cache --no-interaction
 php artisan optimize --no-interaction
 
-php artisan storage:link
+php artisan storage:link || true
 
 touch database/database.sqlite
 php artisan migrate --force
