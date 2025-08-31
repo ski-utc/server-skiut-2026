35a3754de7b51e50d06cc19a71b252d8214af5b8
mathisDlmr <mathis.delmaere@free.fr>
2025-08-31 14:47:52 +0200

    feat(global): add monitoring via prometheus and grafana

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,9 @@ FROM php:8.3-apache
 RUN apt-get update && apt-get install -y \
     git zip unzip curl \
     libzip-dev libpng-dev libonig-dev libxml2-dev \
-    && docker-php-ext-install pdo_mysql mbstring zip exif pcntl
+    && pecl install apcu \
+    && docker-php-ext-install pdo_mysql mbstring zip exif pcntl \
+    && docker-php-ext-enable apcu
 
 # Pour laisser Laravel intercepter les requests
 RUN a2enmod rewrite
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
+use Prometheus\CollectorRegistry;
+use Prometheus\RenderTextFormat;
 
 /*
 Lors de la création d'une nouvelle route, si cette dernière ne fonctionne pas :
@@ -10,6 +11,12 @@ Lors de la création d'une nouvelle route, si cette dernière ne fonctionne pas 
 */
 
 Route::get('/', function () { return view('welcome');})->name('home');
+
+Route::get('/metrics', function (CollectorRegistry $registry) {
+    $renderer = new RenderTextFormat();
+    return response($renderer->render($registry->getMetricFamilySamples()))
+        ->header('Content-Type', RenderTextFormat::MIME_TYPE);
+});
 
 Route::get('/game', [\App\Http\Controllers\ShotgunController::class, 'showGame'])->name('game');
 Route::post('/submit', [\App\Http\Controllers\ShotgunController::class, 'submit'])->name('submit');
