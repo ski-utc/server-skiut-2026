10115f38b49c0d49a98d916ba3f054091f472882
mathisDlmr <mathis.dlmr@gmail.com>
2025-11-02 10:05:54 +0100

    chore: enable Trustproxies middleware on Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $middleware = [
+        \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PrometheusMiddleware::class,
     ];
