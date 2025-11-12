dbd02906e8af1fcbbbbc083c84c954c684dac47c
Mathis Delmaere <mathis.dlmr@etik.com>
2025-11-12 21:09:15 +0100

    ci: fix workflow

--- app/Providers/HealthServiceProvider.php
+++ app/Providers/HealthServiceProvider.php
@@ -25,10 +25,6 @@ class HealthServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (!class_exists(\Spatie\Health\Facades\Health::class)) {
-            return;
-        }
-
         Health::checks([
             // OptimizedAppCheck::new(),
             // DebugModeCheck::new(),
